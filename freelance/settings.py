829f7a7bf08a091968d0c487d146104284911e3d
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-29 10:17:22 +0300

    add rest permission middleware

--- freelance/settings.py
+++ freelance/settings.py
@@ -42,6 +42,15 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'freelance.urls'
 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
