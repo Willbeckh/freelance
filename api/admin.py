7236dc32d1730638e941e06f5603d450cf2b9f7a
kevin <kevinmibz6@gmail.com>
2022-06-29 11:59:07 +0300

    updated models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import User, Job, Room, Message
 # Register your models here.
+
+
+admin.site.register(User)
+admin.site.register(Job)
+admin.site.register(Message)
+admin.site.register(Room)
