d1bf4f186c1fb7b90d44ae814eb55a6cd1a59efd
kevin <kevinmibz6@gmail.com>
2022-06-30 09:28:02 +0300

    updating files

--- freelance/urls.py
+++ freelance/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('', include('api.urls'))
+    path('', include('api.urls')),
 ]
