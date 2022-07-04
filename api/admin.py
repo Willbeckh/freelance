730f8d69dcba2078e041b3e1eef7ea7845273b00
Willbeckh <wileyhndirangu04@gmail.com>
2022-07-04 12:43:21 +0300

    add user profile model to admin site

--- api/admin.py
+++ api/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-from .models import User, Job, Room, Message, Topic, Profile
+from .models import User, Job, Room, Message, Topic, Profile, JobPipeline
+
+
 # Register your models here.
-
-
 admin.site.register(User)
 admin.site.register(Job)
 admin.site.register(Message)
@@ -11,3 +11,15 @@ admin.site.register(Room)
 admin.site.register(Topic)
 admin.site.register(Profile)
 
+
+# registers job pipeline to admin
+class PipelineAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['author']}),
+        ('Title', {'fields': ['job_title']}),
+        ('Status', {'fields': ['job_status']}),
+        ('Applicants', {'fields': ['applicants']}),
+    ]
+
+
+admin.site.register(JobPipeline, PipelineAdmin)
