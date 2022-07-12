0a7d0eaa10b35858dd1dc744186987855b7cb9af
Willbeckh <wileyhndirangu04@gmail.com>
2022-07-13 00:55:23 +0300

    pt:update api root view to swagger docs

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,21 @@
 
 from django.urls import path, include
-# from .views import  LoginView, MessageViewSet, RoomViewSet, JobViewSet, TopicViewSet, ProfileViewSet,CreateUserView
-from .views import *
 from rest_framework import routers
+from .views import *
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view as swagger_get_schema_view
+
+
+# swgger documentation
+schema_view = swagger_get_schema_view(
+    openapi.Info(
+        title='Freelance API',
+        default_version='v1',
+        description='Freelance market API documentation',
+        contact=openapi.Contact(email="freelance.dev@gmail.com"),
+    ),
+    public=True,
+)
 
 router = routers.DefaultRouter()
 router.register('messages', MessageViewSet)
@@ -16,5 +29,6 @@ urlpatterns = [
     path('signup/',CreateUserView.as_view()),
     path('login/',LoginView.as_view(),name='login'),
     path('job/<int:pk>/',JobView.as_view({'get':'retrieve'}),name='job'),
-    path('',include(router.urls))
+    path('api/',include(router.urls)),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-schema'),
 ]
