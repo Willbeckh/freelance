5c4b746f8762127303a697d276de9aeae1b3e7a5
Willbeckh <wileyhndirangu04@gmail.com>
2022-07-13 00:22:09 +0300

    ft: create endpoint for viewing a single job

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 
 from django.urls import path, include
-from .views import  LoginView, MessageViewSet, RoomViewSet, JobViewSet, TopicViewSet, ProfileViewSet,CreateUserView
+# from .views import  LoginView, MessageViewSet, RoomViewSet, JobViewSet, TopicViewSet, ProfileViewSet,CreateUserView
+from .views import *
 from rest_framework import routers
 
 router = routers.DefaultRouter()
@@ -14,5 +15,6 @@ router.register('profile', ProfileViewSet)
 urlpatterns = [
     path('signup/',CreateUserView.as_view()),
     path('login/',LoginView.as_view(),name='login'),
+    path('job/<int:pk>/',JobView.as_view({'get':'retrieve'}),name='job'),
     path('',include(router.urls))
 ]
