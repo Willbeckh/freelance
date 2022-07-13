e98fc704a50e534cc92fd4c79f34421a8bed6c8f
Willbeckh <wileyhndirangu04@gmail.com>
2022-07-13 08:38:02 +0300

    ch: code cleanup

--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,7 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view as swagger_get_schema_view
 
 
+
 # swgger documentation
 schema_view = swagger_get_schema_view(
     openapi.Info(
